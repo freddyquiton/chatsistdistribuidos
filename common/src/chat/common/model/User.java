661b30e7d1412589d498d390587f820bb0ba916a
Pablo Sanabria <pablo.s.q1.4.1991@gmail.com>
2012-09-09 15:19:35 -0400

    Primer commit

--- common/src/chat/common/model/User.java
+++ common/src/chat/common/model/User.java
@@ -1,5 +1,14 @@
 package chat.common.model;
 
 public class User {
-
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	public User()
+	{
+		
+	}
 }
