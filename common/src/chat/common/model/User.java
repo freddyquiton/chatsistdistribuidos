87aa4ded348219a9eb6c1eb3d1d368a917f685f4
Pablo Sanabria <pablo.s.q1.4.1991@gmail.com>
2012-09-09 15:48:30 -0400

    Subida una clase User basica

--- common/src/chat/common/model/User.java
+++ common/src/chat/common/model/User.java
@@ -1,5 +1,7 @@
 package chat.common.model;
 
+import chat.common.tools.Encripter;
+
 public class User {
 	private String username;
 	private String password;
@@ -7,8 +9,56 @@ public class User {
 	private String lastName;
 	private String email;
 	
-	public User()
-	{
-		
+	public User(String theUsername, String thePassword, String theFirstName,
+			String theLastName, String theEmail) {
+		setUsername(theUsername);
+		setPassword(thePassword);
+		setFirstName(theFirstName);
+		setLastName(theLastName);
+		setEmail(theEmail);
 	}
+	
+	// Get methods
+	public String getUsername() {
+		return username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}	
+
+	public String getLastName() {
+		return lastName;
+	}
+	
+	// Set methods	
+	public void setUsername(String theUsername) {
+		username = theUsername;
+	}
+	
+	public void setFirstName(String theFirstName) {
+		firstName = theFirstName;
+	}
+	
+	public void setLastName(String theLastName) {
+		lastName = theLastName;
+	}	
+
+	public void setEmail(String theEmail) {
+		email = theEmail;
+	}
+
+	public void setPassword(String thePassword) {
+		password = Encripter.encryptPassword(thePassword);
+	}
+	
+	public boolean checkPassword(String thePassword)
+	{
+		return password == Encripter.encryptPassword(thePassword);
+	}
+	
 }
